b623551bd23c29f0141785bee0c817e29e603a32
oliviacellinda <oliviacellinda@gmail.com>
2018-10-29 18:14:15 +0700

    first

---
+++ application/views/navbar_top.php
@@ -0,0 +1,48 @@
+<header class="main-header">
+
+	<!-- Logo -->
+	<a href="#" class="logo">
+		<!-- Logo mini untuk sidebar mini, dengan ukuran logo 50x50 piksel -->
+		<span class="logo-mini">TM</span>
+		<!-- Logo ukuran normal untuk tampilan normal dan mobile device-->
+		<span class="logo-lg">Toko Mainan</span>
+	</a>
+
+	<!-- Header Navbar -->
+	<nav class="navbar navbar-static-top" role="navigation">
+		
+		<!-- Navbar right menu -->
+		<div class="navbar-custom-menu">
+			<ul class="nav navbar-nav">
+
+				<!-- User account -->
+				<li class="dropdown user user-menu">
+					
+					<!-- Menu toggle button -->
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+						<img src="<?php echo base_url('assets/image/user.png');?>" class="user-image" alt="Foto Pengguna">
+						<span class="hidden-xs">Nama Admin</span>
+					</a>
+
+					<!-- Dropdown menu -->
+					<ul class="dropdown-menu">
+						<!-- Menu header -->
+						<li class="user-header">
+							<img src="<?php echo base_url('assets/image/user.png');?>" class="img-circle" alt="Foto Pengguna">
+							<p>Nama Admin</p>
+						</li>
+						<!-- Menu footer -->
+						<li class="user-footer">
+							<div>
+								<a href="#" class="btn btn-default btn-flat">Keluar</a>
+							</div>
+						</li>
+					</ul>
+
+				</li>
+
+			</ul>
+		</div>
+
+	</nav>
+</header>
